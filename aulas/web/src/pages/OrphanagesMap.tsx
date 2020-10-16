67f7df2220ef9862eb6686203d508b327c1999ab
Lucas <lucasgenial@hotmail.com>
2020-10-16 18:13:22 -0300

    Segundo commite - Adição da tela dos mapas

--- aulas/web/src/pages/OrphanagesMap.tsx
+++ aulas/web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import 'leaflet/dist/leaflet.css';
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap(){
     return (
-        <div>
-            <h1>OrphanagesMap</h1>
-            <h2>Bem vindos</h2>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no map</h2>
+
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+
+                <footer>
+                    <strong>Porto Seguro</strong>
+                    <span>Bahia</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-16.4354361,-39.0816787]}
+                zoom={15}
+                style={{ width: '100%', height: '100%'}}
+            >
+                <TileLayer
+                    url={'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_KEY_MAPBOX}'}
+                />
+            </Map>
+            
+            <Link to="" className="create-orphanages">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
         </div>
     );
 }
