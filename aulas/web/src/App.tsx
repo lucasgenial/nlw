2ea3f5e140c233a675f7cf21c0435f295bdae909
Lucas <lucasgenial@hotmail.com>
2020-10-16 17:06:16 -0300

    Initial commit

---
+++ aulas/web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// importa a classe principal
+import './styles/global.css';
+
+// Importa o controle das Rotas
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
---
+++ aulas/web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function OrphanagesMap(){
+    return (
+        <div>
+            <h1>OrphanagesMap</h1>
+            <h2>Bem vindos</h2>
+        </div>
+    );
+}
+
+export default OrphanagesMap;
