3ee500b719fed707da50141c0a07ff8d31cb4885
Lucas <lucasgenial@hotmail.com>
2020-10-19 18:10:45 -0300

    Update .yarn-integrity, LICENSE, and 2019 more files...

--- aulas/backend/src/controllers/OrphanagesController.ts
+++ aulas/backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 
+import OrphanageView from '../views/Orphanages_view';
 import Orphanage from '../models/Orphanage';
 
 export default{
@@ -12,7 +14,7 @@ export default{
          relations:['images']
       });
 
-      return response.status(200).json(orphanages);
+      return response.status(200).json(OrphanageView.renderMany(orphanages));
    },
 
    async show(request:Request, response:Response){
@@ -25,7 +27,7 @@ export default{
          relations:['images']
       });
 
-      return response.json(orphanage);
+      return response.json(OrphanageView.render(orphanage));
    },
 
    async create(request:Request, response:Response){
@@ -46,9 +48,36 @@ export default{
          }
       )
 
-      const orphanage = orphangesRepository.create({
-         name, latitude, longitude, about, instructions, opening_hours, open_on_weekends, images
+      const data = {
+         name,
+         latitude,
+         longitude,
+         about,
+         instructions,
+         opening_hours,
+         open_on_weekends,
+         images,
+      };
+
+      const schema = Yup.object().shape({
+         name: Yup.string().required('Nome é obrigatório'),
+         latitude: Yup.number().required('Latitude é obrigatório'),
+         longitude: Yup.number().required('Longitude é obrigatória'),
+         about: Yup.string().required('About é obrigatório').max(300),
+         instructions: Yup.string().required('As instruções são obrigatórias'),
+         opening_hours: Yup.string().required('Horário de Funcionamento é obrigatório'),
+         open_on_weekends: Yup.boolean().required('A informação de abertura aos fins de semana é obrigatória'),
+         images: Yup.array(Yup.object().shape({
+            path: Yup.string().required('O nome do arquivo é obrigatório'),
+         }))
       });
+
+      await schema.validate(data, {
+         // Faz retornar todos os erros de uma só vez
+         abortEarly:false,
+      });
+
+      const orphanage = orphangesRepository.create(data);
 
       await orphangesRepository.save(orphanage);
 
--- aulas/web/src/pages/OrphanagesMap.tsx
+++ aulas/web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import LeaFlet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -9,43 +10,58 @@ import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
 
+const mapIcon = LeaFlet.icon({
+   iconUrl: mapMarkerImg,
+   iconSize: [58, 68],
+   iconAnchor: [29, 68],
+   popupAnchor: [170, 2],
+})
+
 function OrphanagesMap(){
-    return (
-        <div id="page-map">
+      return (
+         <div id="page-map">
             <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy"/>
+               <header>
+                  <img src={mapMarkerImg} alt="Happy"/>
 
-                    <h2>Escolha um orfanato no map</h2>
+                  <h2>Escolha um orfanato no map</h2>
 
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+                  <p>Muitas crianças estão esperando a sua visita :)</p>
+               </header>
 
-
-                <footer>
-                    <strong>Porto Seguro</strong>
-                    <span>Bahia</span>
-                </footer>
+               <footer>
+                  <strong>Porto Seguro</strong>
+                  <span>Bahia</span>
+               </footer>
             </aside>
 
             <Map
-                center={[-16.4354361,-39.0816787]}
-                zoom={15}
-                style={{ width: '100%', height: '100%'}}
+               center={[-16.4354361,-39.0816787]}
+               zoom={15}
+               style={{ width: '100%', height: '100%'}}
             >
-                <TileLayer
-                    url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_KEY_MAPBOX}`}
-                />
-                {/* <TileLayer
-                    url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                /> */}
+               <TileLayer
+                  url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_KEY_MAPBOX}`}
+               />
+               <Marker icon={mapIcon} position={[-16.4354361,-39.0816787]} >
+                  <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                     Lar das Meninas
+                     <Link to="/orphanages/1">
+                        <FiArrowRight size={20} color="#FFF" />
+                     </Link>
+                  </Popup>
+               </Marker>
+
+               {/* <TileLayer
+                  url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               /> */}
             </Map>
-            
-            <Link to="/" className="create-orphanage">
+
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
-        </div>
-    );
+         </div>
+   );
 }
 
 export default OrphanagesMap;
