7247e0e1f531692c72d22547fd26498b8f407400
Lucas Matos e Souza <lucasgenial@hotmail.com>
2020-10-19 02:16:46 -0300

    Update mime, mime.cmd, and 340 more files...

--- aulas/web/src/pages/OrphanagesMap.tsx
+++ aulas/web/src/pages/OrphanagesMap.tsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 import { Map, TileLayer } from 'react-leaflet';
 
+import 'leaflet/dist/leaflet.css';
+
 import mapMarkerImg from '../images/map-marker.svg';
 
-import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap(){
@@ -33,11 +34,14 @@ function OrphanagesMap(){
                 style={{ width: '100%', height: '100%'}}
             >
                 <TileLayer
-                    url={'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_KEY_MAPBOX}'}
+                    url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_KEY_MAPBOX}`}
                 />
+                {/* <TileLayer
+                    url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                /> */}
             </Map>
             
-            <Link to="" className="create-orphanages">
+            <Link to="/" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
